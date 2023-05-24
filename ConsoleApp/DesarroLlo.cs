394996471cd33ac34505865aef60720662db3ac4
Paolo Tello <Paolo Tello>
2023-05-23 23:47:17 -0500

    ingreso nombre

--- ConsoleApp/DesarroLlo.cs
+++ ConsoleApp/DesarroLlo.cs
@@ -8,15 +8,41 @@ namespace ConsoleApp
     {
       
             static void Main(string[] args)
-            {
-                string diactual="";
+        {
+            String nombre = "";
+            string diactual="";
                 DateTime diaactual = DateTime.Today;
                 diactual = diaactual.ToString("d");
                 string[] s = diactual.Split('/');
                 Log.Logger = new LoggerConfiguration()
                     .WriteTo.File("Logs/Log_" + s[2] + s[1] + s[0] + ".txt").CreateLogger();
+           
+            Console.WriteLine("Agregar Nombre:");
 
+            try
+            {
+                Log.Information("insertar nombre");
+                nombre = Console.ReadLine();
+                Console.Read();
+
+              
             }
+            catch (OutOfMemoryException ex)
+            {
+                Log.Error("Error en:" + ex.Message);
+                Console.WriteLine("Error:" + ex.Message);
+            }
+
+            catch (Exception ex)
+            {
+                 Log.Error("Error en:" + ex.Message);
+                Console.WriteLine("Error:" + ex.Message);
+            }
+
+
+
+
+        }
         }
     }
 
