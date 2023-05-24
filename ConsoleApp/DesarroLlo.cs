02c261963d31c6ec66eef7ef9c18cb7f3fe576af
Paolo Tello <Paolo Tello>
2023-05-23 23:38:30 -0500

    log error

---
+++ ConsoleApp/DesarroLlo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using Serilog;
+
+namespace ConsoleApp
+{
+    internal class DesarroLlo
+    {
+      
+            static void Main(string[] args)
+            {
+                string diactual="";
+                DateTime diaactual = DateTime.Today;
+                diactual = diaactual.ToString("d");
+                string[] s = diactual.Split('/');
+                Log.Logger = new LoggerConfiguration()
+                    .WriteTo.File("Logs/Log_" + s[2] + s[1] + s[0] + ".txt").CreateLogger();
+
+            }
+        }
+    }
+
